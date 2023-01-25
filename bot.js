54829ba6162326385c5b9042feefb515b213d43c
Kyle <kyle@hoffpauir.com>
2023-01-25 12:55:24 -0500

    whimsy2

--- bot.js
+++ bot.js
@@ -43,6 +43,23 @@ function postWhimsyMessage(text) {
     "bot_id": botID,
     "text": botResponse
   };
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+    if(res.statusCode == 202) {
+      //neat
+    } else {
+      console.log('rejecting bad status code ' + res.statusCode);
+    }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
 }
 
 function postMessage() {

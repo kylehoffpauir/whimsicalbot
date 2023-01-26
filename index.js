ae5382f75ab43cbbc05e750fbfc86bbf8aa72287
Kyle <kyle@hoffpauir.com>
2023-01-25 19:18:03 -0500

    no auth admin

--- index.js
+++ index.js
@@ -33,11 +33,11 @@ function ping() {
 }
 
 r = require('rethinkdb')
-var username = 'cf484706-5cc6-4430-b04b-4b707ee33f09'
-var password = 'cf01617300586418edb204ab2a9c2757c74ac671'
+var username = 'admin'
+var password = ''
 var name = 'cf484706-5cc6-4430-b04b-4b707ee33f09'
 r.connect({ host: 'cf484706-5cc6-4430-b04b-4b707ee33f09.db.rdb.rethinkdb.cloud',
-      port: 28015, username: username, password: password, name: name}, function(err, conn) {
+      port: 28015, username: username, name: name}, function(err, conn) {
   if(err) throw err;
   r.db('test').tableCreate('tv_shows').run(conn, function(err, res) {
     if(err) throw err;
